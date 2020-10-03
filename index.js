b2a5f847e7e798e0c70575e75c87924cd7426a0c
Chris Skogstad <chris.skogstad1@gmail.com>
2020-10-03 23:17:29 +1000

    added question list, and made it on its own js file to clean things up a bit

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
-
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 // array of questions for user
-const questions = [
 
-];
 
 // function to write README file
 function writeToFile(fileName, data) {
