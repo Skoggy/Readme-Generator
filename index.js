13d95d5b347dc87c6e57a17a65f7e83cca7be169
Chris Skogstad <chris.skogstad1@gmail.com>
2020-10-04 00:12:11 +1000

    I have it working basically, just need to write up the markup format correctly, but it is writing the title down so far

--- index.js
+++ index.js
@@ -5,72 +5,88 @@ const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
+function promptUser() {
+    return inquirer.prompt([
 
-inquirer.prompt([
-
-    {
-        type: "input",
-        name: "title",
-        message: "What is your project title?"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Please describe what your project does?"
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "What are the installation instructions"
-    },
-    {
-        type: "input",
-        name: "usage-info",
-        message: "What is the usage information about your project"
-    },
-    {
-        type: "input",
-        name: "contribution-guidelines",
-        message: "Please enter your contribution guidelines"
-    },
-    {
-        type: "input",
-        name: "test-instructions",
-        message: "What are the test instructions for your project"
-    },
-    {
-        type: "checkbox",
-        message: "Which license type would you like to use",
-        name: "license",
-        choices: [
-            "Apache license 2.0",
-            "Academic Free License v3.0",
-            "GNU General Public License v3.0",
-            "MIT",
-            "Open Software License 3.0"
-        ]
-    },
-    {
-        type: "input",
-        name: "github-username",
-        message: "Please Enter in your GitHub username"
-    },
-    {
-        type: "input",
-        name: "email-address",
-        message: "What is your contact Email?"
-    },
-])
-
-
-// function to write README file
-function writeToFile(fileName, data) {
+        {
+            type: "input",
+            name: "title",
+            message: "What is your project title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please describe what your project does?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the installation instructions"
+        },
+        {
+            type: "input",
+            name: "usage-info",
+            message: "What is the usage information about your project"
+        },
+        {
+            type: "input",
+            name: "contribution-guidelines",
+            message: "Please enter your contribution guidelines"
+        },
+        {
+            type: "input",
+            name: "test-instructions",
+            message: "What are the test instructions for your project"
+        },
+        {
+            type: "checkbox",
+            message: "Which license type would you like to use",
+            name: "license",
+            choices: [
+                "Apache license 2.0",
+                "Academic Free License v3.0",
+                "GNU General Public License v3.0",
+                "MIT",
+                "Open Software License 3.0"
+            ]
+        },
+        {
+            type: "input",
+            name: "github-username",
+            message: "Please Enter in your GitHub username"
+        },
+        {
+            type: "input",
+            name: "email-address",
+            message: "What is your contact Email?"
+        },
+    ])
 }
+
+var generateMarkdown = require('./utils/generateMarkdown');
+
+async function init() {
+    console.log("its working")
+    try {
+        const data = await promptUser();
+
+        const markdown = await generateMarkdown(data);
+
+        await writeFileAsync("README.md", markdown);
+
+        console.log("Successfully wrote your Readme file")
+    } catch (err) {
+        console.log(err);
+    }
+}
+// function to write README file
+//function writeToFile(fileName, data) {
+//}
 
 // function to initialize program
-function init() {
+//function init() {
 
-}
+//}
 
 // function call to initialize program
 init();
